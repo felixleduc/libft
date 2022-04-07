480fdaf1677ce09b2bea4887c0a3b16ce66873f4
Félix-Antoine Leduc <fleduc@student.42quebec.com>
2022-04-07 11:44:48 -0400

    +calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: fleduc <marvin@42quebec.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 09:48:37 by fleduc            #+#    #+#             */
-/*   Updated: 2022/04/01 15:01:04 by fleduc           ###   ########.fr       */
+/*   Updated: 2022/04/07 11:43:34 by fleduc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 
 	buffer = malloc(count * size);
 	if (buffer == NULL)
-		return (0);
+		return (NULL);
 	ft_bzero(buffer, count * size);
-	return ((void *) buffer);
+	return (buffer);
 }
