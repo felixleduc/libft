9c87d45e516e821efb9264ac428b699f686a3b25
Félix-Antoine Leduc <fleduc@student.42quebec.com>
2022-04-07 11:47:07 -0400

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,16 +6,21 @@
 /*   By: fleduc <marvin@42quebec.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 13:08:42 by fleduc            #+#    #+#             */
-/*   Updated: 2022/03/30 13:07:02 by fleduc           ###   ########.fr       */
+/*   Updated: 2022/04/07 11:46:48 by fleduc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*p;
+	size_t	i;
 
-	p = s;
-	while (n-- > 0)
-		*p++ = '\0';
+	i = 0;
+	if (n == 0)
+		return ;
+	while (i < n)
+	{
+		((char *)str)[i] = 0;
+		i++;
+	}
 }
